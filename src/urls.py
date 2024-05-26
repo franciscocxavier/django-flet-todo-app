c0b939f0ee1a087b58083d6c63d3e72328855003
Francisco Xavier <fxaviertemp@gmail.com>
2024-05-26 22:52:10 +0000

    Feature: Add rediction for admin panel

--- src/urls.py
+++ src/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import path, reverse_lazy
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url=reverse_lazy('admin:index'))),
     path('app/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
